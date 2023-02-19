404124bec872c621970c4260fc6f0252769e8197
Jeremiah Chesley <msgrmiah@gmail.com>
2023-02-19 10:15:21 -0700

    changes to make routes and server work properly

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
 const express = require('express');
+const util = require('util');
+const fs = require('fs');
 const path = require('path');
-const notesRoute = require('./routes/notes.js');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use('/api/notes', noteRoute);
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/notes.html'));
-});
+app.use(express.static('public'));
 
+// GET Route for index
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/assets/index.html'))
 );
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/assets/notes.html'))
+);
+
+// Wildcard route to direct users to homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/assets/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
